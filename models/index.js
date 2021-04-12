f686005ae6fc3b9486d145fbe99292033e9906aa
Bella Santiago <bellasantiago.adl@gmail.com>
2021-04-12 18:30:33 +0930

    Models added. Routes file created

--- models/index.js
+++ models/index.js
@@ -7,27 +7,33 @@ const workoutSchema = new Schema({
         type: Date,
         default: Date.now
     },
-    exercises: [{
+    exercises: [
+        {
         type: {
-            
+            type: String,
+            trim: true,
+            required: true
         },
         name: {
-
+            type: String,
+            trim: true,
+            required: true
         },
         duration: {
-
+            type: Number,
+            required: true
         },
         weight: {
-
+            type: Number
         },
         reps: {
-
+            type: Number
         },
         sets: {
-
+            type: Number
         },
         distance: {
-
+            type: Number
         },
 
     }]
