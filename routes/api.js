be937c2fc1cdbe890fabf522e84b501c646af1b0
Bella Santiago <bellasantiago.adl@gmail.com>
2021-04-14 13:26:54 +0930

    Added three routes

--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,16 @@
+const path = require ("path");
 const router = require("express").Router();
-const Workout = require("../models/index.js");
 
 router.get("/", (req, res) => {
     res.sendFile(path.join(_dirname, "../public/index.html"))
+});
+
+router.get("/excercise", (req, res) => {
+    res.sendFile(path.join(_dirname, "../public/exercise.html"))
+});
+
+router.get("/stats", (req, res) => {
+    res.sendFile(path.join(_dirname, "../public/stats.html"))
 });
 
 module.exports = router;
