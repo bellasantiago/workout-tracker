a19d485fa3f7d534ee6afb54660927e0e1821300
Bella Santiago <bellasantiago.adl@gmail.com>
2021-04-19 18:44:34 +0930

    Added final routes

--- routes/api.js
+++ routes/api.js
@@ -1,4 +1,4 @@
-const path = require ("path");
+const path = require("path");
 const router = require("express").Router();
 const { Workout } = require("../models/index.js");
 
@@ -6,7 +6,7 @@ router.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
-router.get("/excercise", function (req, res) {
+router.get("/exercise", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/exercise.html"));
 });
 
@@ -14,8 +14,9 @@ router.get("/stats", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/stats.html"));
 });
 
-router.post("/workouts", ({ body }, res) => {
-    Workout.create(body)
+// Create new workout
+router.post("/workouts", (req, res) => {
+    Workout.create()
         .then(workout => {
             res.json(workout);
         })
@@ -23,5 +24,37 @@ router.post("/workouts", ({ body }, res) => {
             res.status(400).json(err);
         });
 });
+
+// Request all of the workouts
+router.get("/workouts", (req, res) => {
+    Workout.aggregate([
+        {
+            $addFields: {
+                totalDuration: {
+                    $sum: "$exercises.duration"
+                }
+            }
+        }
+    ]).then(result => {
+        res.json(result)
+    })
+        .catch(err => {
+            res.status(400).json(err)
+        })
+});
+
+// Add new exercise to workout
+router.put('/api/workouts/:id', (req, res) => {
+    Workout.findById(req.params.id)
+        .then(workout => {
+            workout.exercises.push(req.body)
+            Workout.updateOne({ _id: req.params.id }, workout, (err, result) => {
+                res.json(workout)
+            })
+        })
+        .catch(err => {
+            res.status(400).json(err)
+        })
+})
 
 module.exports = router;
