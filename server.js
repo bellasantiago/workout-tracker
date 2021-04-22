7d954683355f79477c62506320cf31ed56ed86d5
Bella Santiago <bellasantiago.adl@gmail.com>
2021-04-22 11:07:35 +0930

    deploying

--- server.js
+++ server.js
@@ -10,10 +10,15 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/workout", 
+  {
   useNewUrlParser: true,
-  useFindAndModify: false
-});
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+  useCreateIndex: true
+  }
+);
 
 // routes
 app.use(require("./routes/api.js"));
